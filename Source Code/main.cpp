e8069e0a6d25d86f1f0e5f17ad12966e717d5ff8
Buzzer Felix <101106797+BuzzerFelix@users.noreply.github.com>
2022-03-25 12:37:01 +0200

    EAC Bypass using KDMapepr

--- Source Code/main.cpp
+++ Source Code/main.cpp
@@ -2,23 +2,21 @@
 #include "ConsoleCommands.h"
 #include <conio.h>
 int main() {
+	SetConsoleTitleA((LPCSTR)"EAC Bypass");
 	CheckFileDriver("image.sys");
 	Sleep(5000);
 	ConsoleCommands("kdmapper --mdl image.sys");
-	Sleep(2500);
-	while (true) 
+	if (_getch() == 'r')
 	{
-		if (_getch() == VK_F6) 
-		{
-			cout << ("Exiting...");
-			Sleep(6000);
-			exit(21);
-		}
-		else 
-		{
-			cout << ("Maybe its wrong Key :)");
-			return true;
-		}
+		cout << ("Exiting...");
+		Sleep(6000);
+		exit(21);
+	}
+	else
+	{
+		cout << ("Maybe its wrong Key :)");
+		Sleep(10000);
+		exit(34);
 	}
 	return 0;
 }
